bkatyl/googet

commit d2888e064a467bc7aff8213bb44d0c606b54d02f

Use strings.EqualFold for efficient string comparison. (#82)
Addresses static check error SA6005: https://staticcheck.io/docs/checks#SA6005

googet.goospec
@@ -1,4 +1,4 @@
-{{$version := "2.17.4@1" -}}
+{{$version := "2.17.5@1" -}}
 {
   "name": "googet",
   "version": "{{$version}}",
@@ -13,6 +13,7 @@
     "path": "install.ps1"
   },
   "releaseNotes": [
+    "2.17.5 - Use strings.EqualFold for string comparison",
     "2.17.4 - Removing x86 detection error message",
     "2.17.3 - Retry once on any Get failure",
     "2.17.2 - Add Source field in GooGet Spec",

googet_rmrepo.go
@@ -61,7 +61,7 @@ func (cmd *rmRepoCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface
 	var foundRepo repoFile
 	for _, rf := range rfs {
 		for _, re := range rf.repoEntries {
-			if strings.ToLower(re.Name) == strings.ToLower(name) {
+			if strings.EqualFold(re.Name, name) {
 				foundRepo = rf
 				break
 			}
@@ -75,7 +75,7 @@ func (cmd *rmRepoCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface
 
 	var res []repoEntry
 	for _, re := range foundRepo.repoEntries {
-		if strings.ToLower(re.Name) != strings.ToLower(name) {
+		if strings.EqualFold(re.Name, name) {
 			res = append(res, re)
 		}
 	}

commit 8b3609b024a81cf57ce13f972d0e883bfdfa48bf

Fixing rerepo bug that removes all other repos from a repo file
Fixing rerepo bug that removes all other repos from a repo file.
- This regression was introduced in https://github.com/google/googet/pull/82

googet.goospec
@@ -1,4 +1,4 @@
-{{$version := "2.18.3@0" -}}
+{{$version := "2.18.4@0" -}}
 {
   "name": "googet",
   "version": "{{$version}}",
@@ -15,6 +15,7 @@
     "path": "install.ps1"
   },
   "releaseNotes": [
+    "2.18.4 - Fixed bug removing repo. All other repos were removed.",
     "2.18.2 - Rename Oauth credential configuration and manually set Authorization header in requests.",
     "2.18.0 - Add support for passing Oauth credentials with GooGet requests.",
     "2.17.5 - Use strings.EqualFold for string comparison",

googet_rmrepo.go
@@ -32,7 +32,7 @@ func (*rmRepoCmd) Name() string     { return "rmrepo" }
 func (*rmRepoCmd) Synopsis() string { return "remove repository" }
 func (*rmRepoCmd) Usage() string {
 	return fmt.Sprintf(`%s rmrepo <name>:
-	Removes the named repository from GooGet's repository list. 
+	Removes the named repository from GooGet's repository list.
 `, filepath.Base(os.Args[0]))
 }
 
@@ -75,7 +75,7 @@ func (cmd *rmRepoCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface
 
 	var res []repoEntry
 	for _, re := range foundRepo.repoEntries {
-		if strings.EqualFold(re.Name, name) {
+		if !strings.EqualFold(re.Name, name) {
 			res = append(res, re)
 		}
 	}